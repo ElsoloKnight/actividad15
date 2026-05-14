1af41ca22dbf5778cb4dd6557293e6eaecff7bbd
ElsoloKnight <juaneio@hotmail.com>
2026-05-13 21:25:50 -0600

    base act 6-8

--- app/src/main/java/com/example/adaptador/MainActivity.kt
+++ app/src/main/java/com/example/adaptador/MainActivity.kt
@@ -5,21 +5,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.adaptador.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
+    private fun getNews(): List<Noticia> {
+        val news = listOf<Noticia>(
+            Noticia("Noticia 1", "Se inundó periferico"),
+            Noticia("Noticia 2", "Perdio el america")
+        )
+        return news
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding.rvwNews.layoutManager = LinearLayoutManager(this)
+        binding.rvwNews.adapter = NoticiaAdapter(getNews())
     }
-}
+
+}<
--- app/src/main/java/com/example/adaptador/NoticiaAdapter.kt
+++ app/src/main/java/com/example/adaptador/NoticiaAdapter.kt
@@ -1,21 +1,28 @@
 package com.example.adaptador
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.adaptador.databinding.ItemListaBinding
 
 class NoticiaAdapter(val noticias: List<Noticia>) : RecyclerView.Adapter<NoticiaHolder>() {
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): NoticiaHolder {
-        TODO("Not yet implemented")
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemListaBinding.inflate(inflater, parent, false)
+        return NoticiaHolder(binding)
     }
 
     override fun onBindViewHolder(
         holder: NoticiaHolder,
         position: Int
     ) {
-        TODO("Not yet implemented")
+
+        val noticia = noticias[position]
+        holder.binding.tvwTittle.text = noticia.titulo
+        holder.binding.tvwDescription.text = noticia.descripcion
     }
 
     override fun getItemCount() = noticias.size
